181468cce5d7509465d7342b1144273a52a928cc
liuda <liudmyla.iovenko@gmail.com>
2021-12-08 22:34:46 -0500

    [+] Added support for the Plus/Minus button (+/-) and a period to indicate the decimal place (.)

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,16 @@ function clickNumber(num) {
 function clearDisplay() {
     document.getElementById("calc-display").value = "";
 }
-
-
+function plusMinus() {
+    if (document.getElementById("calc-display").value !== ""){
+        document.getElementById("calc-display").value *=-1;
+    }
+}
+function decimalPlace() {
+    let value = document.getElementById("calc-display").value;
+    if (value === ""){
+        document.getElementById("calc-display").value = "0.";
+    } else if (value.indexOf('.') === -1) {
+        document.getElementById("calc-display").value += ".";
+    }
+}
